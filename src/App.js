e0522c5b74185077838efb2721cd843e0f7eb62a
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-28 13:59:50 +0200

    App and index aligned

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
 
 const App = () => {
-  console.log({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  });
-  return <div className='App'></div>;
+  return <div className='App'>Welcome</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// REACT ROUTER
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+import { SignupPage } from "./components/Layout/auth/SignupPage";
+import { SigninPage } from "./components/Layout/auth/SigninPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    exact: true,
+    element: <App />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <SigninPage />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
